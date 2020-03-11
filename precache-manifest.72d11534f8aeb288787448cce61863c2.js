a3a5873b0a37904cdbd1c4d2feb13e93366f31e0
Mutiullah Samim <mutiullah.samim@gmail.com>
2020-03-11 12:51:11 +0800

    Updates

---
+++ precache-manifest.72d11534f8aeb288787448cce61863c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769492a8980fd3622c90b7df7a078bba",
+    "url": "/bejak-movies/index.html"
+  },
+  {
+    "revision": "3974309b22647546d6a6",
+    "url": "/bejak-movies/static/css/2.e4708966.chunk.css"
+  },
+  {
+    "revision": "895d13e2c729f271e2da",
+    "url": "/bejak-movies/static/css/main.1d515c38.chunk.css"
+  },
+  {
+    "revision": "3974309b22647546d6a6",
+    "url": "/bejak-movies/static/js/2.538df52e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/bejak-movies/static/js/2.538df52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895d13e2c729f271e2da",
+    "url": "/bejak-movies/static/js/main.0dd41b7f.chunk.js"
+  },
+  {
+    "revision": "50c3f0b631db417197e7",
+    "url": "/bejak-movies/static/js/runtime-main.9f5e0886.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bejak-movies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bejak-movies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bejak-movies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bejak-movies/static/media/slick.f97e3bbf.svg"
+  }
+]);
